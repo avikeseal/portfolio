a3c7e348b176aa0f7d7aca560f911f306673fd8b
Avike Seal <avikeseal@gmail.com>
2025-11-26 09:40:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+burger.addEventListener('click', () => {
+    burger.classList.toggle('open');
+    menubar.classList.toggle('open');
+});
+
+const burgerMenu = document.querySelector('.nav-item');
+nav-item.addEventListener('click', () => {
+    nav-item.classList.toggle('open')// here target your navbar and change a class, an attribute or whatever you want
+})
