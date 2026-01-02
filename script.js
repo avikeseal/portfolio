61c58444bda8641e213c84bf0995b308fda9d860
Avike Seal <avikeseal@gmail.com>
2026-01-02 04:27:43 -0500

    responsive features added to css

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ const burgerMenu = document.querySelector('.nav-item');
 nav-item.addEventListener('click', () => {
     nav-item.classList.toggle('open')// here target your navbar and change a class, an attribute or whatever you want
 })
+
+const element = document.getElementById('skull-icon');
+const degree = 180; // The desired angle
+element.style.transform = 'rotate(' + degree + 'deg)';
